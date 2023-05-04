7feb966bae0e597e86556ee9a4fbde177b0a06bb
Shanu J Panicker <shanujpanicker@terveystech.com>
2023-05-04 18:42:36 +0530

    first commit

---
+++ nifti2dicom/nifti2dicom/main.py
@@ -0,0 +1,4 @@
+from nifti2dicom import nifti2dicom_1file
+
+
+nifti2dicom.nifti2dicom_1file(r"C:\Users\shanu\practice files\nifti2dicom\ds114_sub009_highres.nii",r"C:\Users\shanu\practice files\nifti2dicom\dicom") 
---
+++ nifti2dicom/nifti2dicom/nifti2dicom.py
@@ -0,0 +1,56 @@
+import SimpleITK as sitk
+import os
+import time
+from glob import glob
+
+
+def writeSlices(series_tag_values, new_img, i, out_dir):
+    image_slice = new_img[:,:,i]
+    writer = sitk.ImageFileWriter()
+    writer.KeepOriginalImageUIDOn()
+
+    # Tags shared by the series.
+    list(map(lambda tag_value: image_slice.SetMetaData(tag_value[0], tag_value[1]), series_tag_values))
+
+    # Slice specific tags.
+    image_slice.SetMetaData("0008|0012", time.strftime("%Y%m%d")) # Instance Creation Date
+    image_slice.SetMetaData("0008|0013", time.strftime("%H%M%S")) # Instance Creation Time
+
+    # Setting the type to CT preserves the slice location.
+    image_slice.SetMetaData("0008|0060", "CT")  # set the type to CT so the thickness is carried over
+
+    # (0020, 0032) image position patient determines the 3D spacing between slices.
+    image_slice.SetMetaData("0020|0032", '\\'.join(map(str,new_img.TransformIndexToPhysicalPoint((0,0,i))))) # Image Position (Patient)
+    image_slice.SetMetaData("0020,0013", str(i)) # Instance Number
+
+    # Write to the output directory and add the extension dcm, to force writing in DICOM format.
+    writer.SetFileName(os.path.join(out_dir,'slice' + str(i).zfill(4) + '.dcm'))
+    writer.Execute(image_slice)
+
+
+def nifti2dicom_1file(in_dir, out_dir):
+    """
+    This function is to convert only one nifti file into dicom series
+
+    `nifti_dir`: the path to the one nifti file
+    `out_dir`: the path to output
+    """
+
+    os.makedirs(out_dir, exist_ok=True)
+
+    new_img = sitk.ReadImage(in_dir) 
+    modification_time = time.strftime("%H%M%S")
+    modification_date = time.strftime("%Y%m%d")
+
+    direction = new_img.GetDirection()
+    series_tag_values = [("0008|0031",modification_time), # Series Time
+                    ("0008|0021",modification_date), # Series Date
+                    ("0008|0008","DERIVED\\SECONDARY"), # Image Type
+                    ("0020|000e", "1.2.826.0.1.3680043.2.1125."+modification_date+".1"+modification_time), # Series Instance UID
+                    ("0020|0037", '\\'.join(map(str, (direction[0], direction[3], direction[6],# Image Orientation (Patient)
+                                                        direction[1],direction[4],direction[7])))),
+                    ("0008|103e", "Created-Pycad")] # Series Description
+
+    # Write slices to output directory
+    list(map(lambda i: writeSlices(series_tag_values, new_img, i, out_dir), range(new_img.GetDepth())))
+
